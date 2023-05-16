d335c401c7f0db71736907e695be2dad9f4be98b
Yuvakiran Yatam <saimallareddy30@gmail.com>
2023-05-16 11:59:33 +0530

    Add project files.

---
+++ SymmetricTree/MirrorCheck.cs
@@ -0,0 +1,16 @@
+﻿namespace SymmetricTree;
+
+public class MirrorCheck
+{
+    public bool IsSymmetric(TreeNode? node)
+    {
+        return node != null && IsMirror(node.Left, node.Right);
+    }
+
+    public bool IsMirror(TreeNode? node1, TreeNode? node2)
+    {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null || node1.Value != node2.Value) return false;
+            return IsMirror(node1.Left, node2.Right) && IsMirror(node1.Right, node2.Left);
+    }
+}
---
+++ SymmetricTree/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SymmetricTree;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TreeNode root = new(1)
+        {
+            Left = new TreeNode(2),
+            Right = new TreeNode(2)
+        };
+        root.Left.Left = new TreeNode(3);
+        root.Left.Right = new TreeNode(4);
+        root.Right.Left = new TreeNode(4);
+        root.Right.Right = new TreeNode(3);
+
+        var mirrorCheck = new MirrorCheck();
+        var isSymmetric = mirrorCheck.IsSymmetric(root);
+        Console.WriteLine($"Is Binary tree symmetric {isSymmetric}");
+    }
+}
